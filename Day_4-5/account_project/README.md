1f6a0e631fa31b28cfe60e2a88bd574571ce0dc1
hoangdz1005 <nnh10052002@gmail.com>
2024-03-22 16:19:42 +0700

    day 4-5

---
+++ Day_4-5/account_project/README.md
@@ -0,0 +1,14 @@
+# Day 4 + 5
+## Demo Login + Register
+### Giao diện khi người dùng truy cập 
+![image](./images/1.png)
+### Nếu chưa có tài khoản chọn Create Account, hoặc có tài khoản mà quyên mật khẩu chọn forgot password, ứng dụng điều hướng đến trang đăng kí 
+![image](./images/2.png)
+### Nhập thông tin, click vào create account, tạo thành công ứng dụng điều hướng lại trang đăng nhập 
+![image](./images/3.png)
+### Trang đăng nhập thành công
+![image](./images/4.png)
+### Bắt lỗi đăng kí 
+![image](./images/6.png)
+### Bắt lỗi đăng nhập
+![image](./images/5.png)
---
+++ Day_4-5/account_project/lib/model/res/app_strings.dart
@@ -0,0 +1,17 @@
+import 'package:account_project/main.dart';
+
+class AppStrings {
+  static const appTitle = "Flutter Demo";
+  static const loginTitle = "Login Page";
+  static const registerTitle = "Register Page";
+  static const loginSuccessTitle = "Login Success Page";
+  static const logoText = "FLUTTER";
+  static const emailLabel = "Enter your email...";
+  static const passwordLabel = "Enter your password...";
+  static const rePasswordLabel = "Re-enter your password...";
+  static const notifyLoginFailed = "Email or Passwords wrong!";
+  static const notifyRegisterFailed = "Passwords do not match!";
+  static const forgotPassword = "You forgot password?";
+  static const textFormRegister = "Already have an account?";
+  static const textSuccess = "Welcome tom my application";
+}
